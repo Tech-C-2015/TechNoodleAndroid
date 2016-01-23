8910834e4c851b1c00564279655e9b1946f4a02a
shokei125 <shokei125@gmail.com>
2016-01-23 16:20:28 +0900

    merge from id ccc7703

--- app/src/main/java/com/example/teacher/technoodleandroid/MainActivity.java
+++ app/src/main/java/com/example/teacher/technoodleandroid/MainActivity.java
@@ -1,19 +1,71 @@
 package com.example.teacher.technoodleandroid;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.example.teacher.technoodleandroid.util.GetRequest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MainActivity extends ActionBarActivity {
+    EditText editWord;
+    TextView txtResult;
+    Button btnGet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editWord = (EditText) findViewById(R.id.edit_word);
+        txtResult = (TextView) findViewById(R.id.txt_result);
+        btnGet = (Button) findViewById(R.id.btn_get);
+        editWord.setText("東京");
+        //click監視
+        btnGet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getBtn(v);
+            }
+        });
+
     }
 
+    //get送信
+    protected void getBtn(View view) {
+        Map<String, String> params = makeParameter();
+        AppController app = (AppController) getApplication();
+        GetRequest request = new GetRequest(new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //実際のjsonデータはresponseに格納されてる
+                txtResult.setText(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }, params);
+        app.mRequestQueue.add(request);
+    }
+
+    //editwordからMapにkeyとvalueのセット
+    private Map<String, String> makeParameter() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", editWord.getText().toString());
+        return params;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,4 +88,5 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
---
+++ app/src/main/java/com/example/teacher/technoodleandroid/util/GetRequest.java
@@ -0,0 +1,39 @@
+package com.example.teacher.technoodleandroid.util;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+import java.util.Map;
+
+public class GetRequest extends StringRequest {
+
+    private Map<String, String> mParams;
+    //API　URLの設置
+    private static final String URL = "http://133.130.106.164/Tech-Noodle-Api/public/noodle/list";
+
+    public GetRequest(Response.Listener<String> listener, Response.ErrorListener errorListener, Map<String, String> mParams) {
+        super(Method.GET, URL, listener, errorListener);
+        this.mParams = mParams;
+    }
+
+    @Override
+    public String getUrl() {
+        return super.getUrl() + makeGetParameter();
+    }
+
+    /*
+    パラメータの作成
+     */
+    private String makeGetParameter() {
+        String param = "?";
+        boolean isFirst = true;
+
+        for (Map.Entry<String, String> entry : mParams.entrySet()) {
+            if (isFirst) {
+                param += entry.getKey() + "=" + entry.getValue();
+            } else {
+                param += "&" + entry.getKey() + "=" + entry.getValue();
+            }
+        }
+        return param;
+    }
+}
